2a0f7fd003369c7bc7620f94943a9f71af90bf1c
Peter Bartels <peterbartels@gmail.com>
2013-05-04 16:57:03 +0100

    Added GStandardImport project

---
+++ GStandardImport/GStandardSchemaReader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class GStandardSchemaReader
+    {
+        private string contents;
+
+        public GStandardSchemaReader(string tempPath, string fileName)
+        {
+            using (var client = new WebClient())
+            {
+                var cachedFile = Path.Combine(tempPath, "cached_files", fileName + ".html");
+                if (!File.Exists(cachedFile))
+                {
+                    string s = client.DownloadString(@"http://www.z-index.nl/g-standaard/beschrijvingen/technisch/Bestanden/bestand?bestandsnaam="+fileName);
+                    File.WriteAllText(cachedFile, s);
+                }
+                contents = File.ReadAllText(cachedFile);
+            }
+        }
+
+        public string GetName()
+        {
+            var match = Regex.Match(contents, @"Bestand\s\d\d\d(.*?)\<", RegexOptions.IgnoreCase);
+
+            var name = match.Groups[1].ToString().Trim();
+            name = name.Replace(' ', '_');
+            name = name.Replace('-', '_');
+            name = Regex.Replace(name, "[^A-Za-z0-9 _]", "");
+            return name;
+        }
+
+        public List<FlatFileColumnInfo> GetFlatFileColumnInfo()
+        {
+
+            var matches = Regex.Matches(contents, @"\<tr\sclass=""(odd|even)"".*?\>(.*?)<\/tr\>", RegexOptions.Singleline);
+            var dataInfoList = new List<FlatFileColumnInfo>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                var row = matches[i].Groups[2].ToString();
+                var colname = Regex.Match(row, @"\<th\>\<.*?\>(.*?)\<", RegexOptions.IgnoreCase).Groups[1].ToString();
+
+                var metaDataMatches = Regex.Matches(row, @"\<td.*?\>(.*?)\<", RegexOptions.IgnoreCase);
+                var length = int.Parse(Regex.Replace(metaDataMatches[2].Groups[1].ToString(), @"\(.*?\)", ""));
+                var description = metaDataMatches[0].Groups[1].ToString();
+                description = description.Replace(' ', '_');
+                description = description.Replace('-', '_');
+                colname = Regex.Replace(description, "[^A-Za-z0-9 _]", "") + "_" + colname;
+                
+                if (colname == "Leeg_veld_")
+                {
+                    colname = Regex.Replace(Guid.NewGuid().ToString(), "^[0-9]*", "").Replace('-','_');
+                }
+                var positions = metaDataMatches[4].Groups[1].ToString();
+                var posstart = int.Parse(positions.Split('-')[0]);
+                var posend = int.Parse(positions.Split('-')[1]);
+                dataInfoList.Add(new FlatFileColumnInfo() { ColName = colname, Length = length, PosStart = posstart, PosEnd = posend });                
+            }
+            return dataInfoList;
+        }
+    }
+}
---
+++ GStandardImport/ImportForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestProject
+{
+    partial class ImportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImportButton = new System.Windows.Forms.Button();
+            this.FolderLbl = new System.Windows.Forms.Label();
+            this.folderLocationLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Location = new System.Drawing.Point(251, 42);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(75, 23);
+            this.ImportButton.TabIndex = 0;
+            this.ImportButton.Text = "Start Import";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            this.ImportButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FolderLbl
+            // 
+            this.FolderLbl.AutoSize = true;
+            this.FolderLbl.Location = new System.Drawing.Point(13, 13);
+            this.FolderLbl.Name = "FolderLbl";
+            this.FolderLbl.Size = new System.Drawing.Size(39, 13);
+            this.FolderLbl.TabIndex = 1;
+            this.FolderLbl.Text = "Folder:";
+            // 
+            // folderLocationLabel
+            // 
+            this.folderLocationLabel.AutoSize = true;
+            this.folderLocationLabel.Location = new System.Drawing.Point(59, 13);
+            this.folderLocationLabel.Name = "folderLocationLabel";
+            this.folderLocationLabel.Size = new System.Drawing.Size(0, 13);
+            this.folderLocationLabel.TabIndex = 2;
+            
+            // 
+            // ImportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 80);
+            this.Controls.Add(this.folderLocationLabel);
+            this.Controls.Add(this.FolderLbl);
+            this.Controls.Add(this.ImportButton);
+            this.Name = "ImportForm";
+            this.Text = "Import G-Standard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ImportButton;
+        private System.Windows.Forms.Label FolderLbl;
+        private System.Windows.Forms.Label folderLocationLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
