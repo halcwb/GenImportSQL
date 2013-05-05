4fa452f7eb754ec82ec60a0ac50420a5759276cf
Peter Bartels <peterbartels@gmail.com>
2013-05-05 19:46:36 +0100

    New SQL files

--- GenerateFormulariumXml.sql
+++ GenerateFormulariumXml.sql
@@ -1,21 +1,17 @@
-SELECT TOP 10
-  RTRIM(IG.Groepnaam_GRPNAM) GroepNaam,
+SELECT  top 3 IG.Naam,
 
-  (SELECT TOP 5 RTRIM(GN.Generieke_naam_GNGNAM)
+  (SELECT 
+	Naam,
 	
-	FROM Namen
-	JOIN Artikelen A ON A.Artikelnaamnummer_ATNMNR = Namen.Naamnummer_NMNR
-	JOIN Handelsproducten HP ON A.HandelsProduktKode_HPK_HPKODE = HP.HandelsProduktKode_HPK_HPKODE
-	JOIN Ingegeven_samenstellingen ingsamenst ON ingsamenst.HandelsProduktKode_HPK_HPKODE = HP.HandelsProduktKode_HPK_HPKODE 
-	JOIN Generieke_namen GN on GN.GeneriekeNaamKode_GNK_GNGNK =  ingsamenst.GeneriekeNaamKode_GNK_GNGNK
-	WHERE 
-		 HP.FTK_1_GRP001 = IG.Groepkode_GRPINP OR
-		 HP.FTK_2_GRP002 = IG.Groepkode_GRPINP OR
-		 HP.FTK_3_GRP003 = IG.Groepkode_GRPINP OR
-		 HP.FTK_4_GRP004 = IG.Groepkode_GRPINP OR
-		 HP.FTK_5_GRP005 = IG.Groepkode_GRPINP 
-    FOR XML PATH('Generieken'), TYPE)
+	(SELECT 
+		*/*,
+		(SELECT * FROM Products(SR.GPKCode) FOR XML PATH('Products'), TYPE)*/
+		
+		FROM ShapeRoute(G.Code) SR FOR XML PATH('ShapeRoute'), TYPE)
+	
+	FROM Generiek(IG.Groepkode_GRPINP) G
+    FOR XML PATH('Generiek'), ROOT('Generieken'), TYPE)
 
-  FROM  Informatorium_groepen IG 
+  FROM  InformatoriumGroep IG 
 
-  FOR XML PATH('InformatoriumGroepen'), ROOT('XMLFormularium')
+  FOR XML PATH('InformatoriumGroep'), ROOT('XMLFormularium')
